ae008f0ffb23292883f99bbf031c2a67d2adfe76
kbburk-TSF <kevin@temporalinertia.com>
2025-09-27 19:48:01 -0500

    twocharts fix

---
+++ src/main-twocharts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import TwoCharts from "./tabs/TwoCharts.jsx";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<TwoCharts />);
